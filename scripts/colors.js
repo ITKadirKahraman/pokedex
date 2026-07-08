94a54f3a08d152e64993d5eeea81a8e43727cb23
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-08 23:39:13 +0200

    update colors.js

---
+++ scripts/colors.js
@@ -0,0 +1,22 @@
+const typeColors = {
+
+    fire: "var(--type-fire)",
+    water: "var(--type-water)",
+    grass: "var(--type-grass)",
+    electric: "var(--type-electric)",
+    psychic: "var(--type-psychic)",
+    ice: "var(--type-ice)",
+    dragon: "var(--type-dragon)",
+    dark: "var(--type-dark)",
+    fairy: "var(--type-fairy)",
+    normal: "var(--type-normal)",
+    fighting: "var(--type-fighting)",
+    poison: "var(--type-poison)",
+    ground: "var(--type-ground)",
+    flying: "var(--type-flying)",
+    bug: "var(--type-bug)",
+    rock: "var(--type-rock)",
+    ghost: "var(--type-ghost)",
+    steel: "var(--type-steel)",
+
+};
