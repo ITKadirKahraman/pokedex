bd174eff31430e7c104c4f1919cea40b580f5d7c
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-15 23:49:12 +0200

    modify script

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,28 @@
-const BASE_URL = "https://pokeapi.co/api/v2/";
-const LOAD_COUNT = 20;
-let layoutPokemon = [];
-let allPokemons = [];
-let currentIndex = 0;
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+const pokemonInput = document.getElementById('pokemonInput');
+let allPokemons = []; // Cache von allen geladenen Pokémons
+let searchedPokemons = []; // die gesuchten Pokémons
+let currentIndex = 0; // welches Pokemon ist gerade im Dialog geöfnnet
+let contentRenderd = false; // Inhalt wurde noch nicht gerendert
+const LOAD_COUNT = 20; // wie viele auf einmal laden
+const MAX_AMOUNT = 1025; // maximale Anzahl von Pokemon
 
 function init() {
+    renderPage();
+}
+
+function renderPage() {
     renderHeader();
-    fetchDataJson();
+    fetchFirstTwentyPokemons();
     renderLoadMoreContainer();
     renderFooter();
 }
 
-function renderHeader() {
-    document.getElementById('headerContainer').innerHTML = getHeader();
+function clearMessageMinLetter() {
+    document.getElementById('NoPokemonsFound').innerHTML = "";
 }
 
-function renderFooter() {
-    document.getElementById('footerContainer').innerHTML = getFooter();
-}
-
-function renderSmallCards() {
-    document.getElementById('pokemonList').innerHTML = getSmallPokemonCards();
-}
-
-function renderLoadMoreContainer() {
-    document.getElementById('loadMoreContainer').innerHTML = getLoadMoreButton();
-}
-
-function renderDialog() {
-    document.getElementById('dialog').innerHTML = getDialogPokemonCard();
-}
-
-function openDialog() {
-    renderDialog()
-}
-
-async function fetchDataJson() {
+async function fetchFirstTwentyPokemons() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
     const data = await response.json();
 
@@ -45,18 +32,37 @@ async function fetchDataJson() {
         const response = await fetch(pokemon.url);
         const details = await response.json();
 
-        layoutPokemon.push(details);
+        allPokemons.push(details);
     }
 
-    console.log(data);
     renderLayoutPokemon();
 }
 
-async function renderLoadMorePokemon() {
+async function loadMorePokemons() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
     const data = await response.json();
 
+    for (let index = LOAD_COUNT + 1; index < MAX_AMOUNT; index++) {
+        const element = array[index];
+        
+    }
     
+}
+
+async function searchPokemon() {
+    let search = document.getElementById('pokemonInput');
+    searchedPokemons = [];
+
+    search.addEventListener("keydown", function (event) {
+        if(event.key === "Enter") {
+            renderLayoutPokemon();
+            searchedPokemons.push(search);
+        }
+    })
+}
+
+async function loadMoreBtn() {
+    loadMorePokemons();
 }
 
 async function postData(url = "", data = {}) {
@@ -73,31 +79,7 @@ async function postData(url = "", data = {}) {
 function renderLayoutPokemon() {
     let content = document.getElementById("pokemonList");
     content.innerHTML = '';
-    for (let index = 0; index < layoutPokemon.length; index++) {
-        content.innerHTML += getSmallPokemonCards(layoutPokemon[index]);
+    for (let index = 0; index < LOAD_COUNT; index++) {
+        content.innerHTML += getSmallPokemonCards(allPokemons[index], index);
     }
-}
-
-function arrowLeft() {
-    let dialogLeft = document.getElementById('arrowLeft');
-    
-    dialogLeft.addEventListener("click", () => {
-        currentIndex--;
-        if(currentIndex < 0) {
-            currentIndex = allPokemons.length - 1;
-        }
-        renderDialog();
-    });
-}
-
-function arrowRight() {
-    let dialogRight = document.getAnimations('arrowRight');
-    
-    dialogRight.addEventListener("click", () => {
-        currentIndex++;
-        if (currentIndex >= allPokemons.length) {
-            currentIndex = 0;
-        }
-        renderDialog();
-    });
 }
