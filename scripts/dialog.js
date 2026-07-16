8c7eb9aa1c9235fdbb035d0929c67aa01f02aae7
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-16 21:15:17 +0200

    modify dialog

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,16 +1,55 @@
-function openDialog(index) {
-    console.log(index);
-    console.log(allPokemons[index]);
-    
+async function openDialog(index) {
     currentIndex = index;
+    const pokemon = allPokemons[currentIndex];
     const dialog =  document.getElementById('dialog');
-    dialog.innerHTML = getDialogPokemonCard(allPokemons[currentIndex]);
+    dialog.innerHTML = getDialogPokemonCard(pokemon);
     dialog.showModal();
+    await loadEvolutionChain(pokemon)
 }
 
-function renderDialog() {
+async function loadEvolutionChain(pokemon) {
+    const speciesResponse = await fetch(pokemon.species.url);
+    const speciesData = await speciesResponse.json();
+
+    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionResponse.json();
+
+    const names = getEvolutionNames(evolutionData.chain);
+
+    const container =
+        document.getElementById(`evolutionChain${pokemon.id}`);
+
+    container.innerHTML = "";
+
+    for (const name of names) {
+
+        const response = await fetch(BASE_URL + name);
+
+        const evolutionPokemon = await response.json();
+
+        container.innerHTML += evolutionChain(evolutionPokemon);
+    }
+}
+
+function getEvolutionNames(chain) {
+
+    const evolutions = [];
+
+    while (chain) {
+
+        evolutions.push(chain.species.name);
+
+        chain = chain.evolves_to[0];
+    }
+
+    return evolutions;
+}
+
+async function renderDialog() {
     const dialog = document.getElementById('dialog');
-    dialog.innerHTML = getDialogPokemonCard(allPokemons[currentIndex]);
+    const pokemon = allPokemons[currentIndex];
+    dialog.innerHTML = getDialogPokemonCard(pokemon);
+    await loadEvolutionChain(pokemon);
 }
 
 function closeDialog() {
