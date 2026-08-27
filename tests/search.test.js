050bd222554e6cb7fb8a0c4e45ad2dedda4a1f06
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-08-27 23:55:18 +0200

    the test for search

---
+++ tests/search.test.js
@@ -0,0 +1,11 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { search } from "./search.js";
+
+test.only("searchs pokemons", () => {
+    selectedType, 
+    displayedPokemons, 
+    allPokemons, 
+    loadedCount
+    assert.deepStrictEqual(search());
+});
