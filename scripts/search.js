382165179ffdcc2f5d0391555afc822d10294a08
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-17 21:50:17 +0200

    delete lines

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,5 @@
 function initSearch() {
     const search = document.getElementById('pokemonInput');
-
     search.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
             searchPokemon();
@@ -11,25 +10,20 @@ function initSearch() {
 function searchPokemon() {
     const input = document.getElementById("pokemonInput");
     const searchTerm = input.value.toLowerCase().trim();
-
     if (searchTerm.length < 3) {
         displayedPokemons = allPokemons.slice(0, loadedCount);
         renderLayoutPokemon();
         return;
     }
-
     displayedPokemons = allPokemons.filter(pokemon =>
         pokemon.name.toLowerCase().includes(searchTerm)
     );
-
     renderLayoutPokemon();
 }
 
 function filterTypePokemon() {
-
     const selectedType =
         document.getElementById("typeFilter").value;
-
     if (selectedType === "all") {
         displayedPokemons = allPokemons.slice(0, loadedCount);
     } else {
@@ -41,6 +35,5 @@ function filterTypePokemon() {
                 )
             );
     }
-
     renderLayoutPokemon();
 }
