ce2f5cc306a6703116da7572375c94cb889bc7eb
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-22 22:21:05 +0200

    clean lines and added one more function

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,53 +1,50 @@
 async function openDialog(index) {
     currentIndex = index;
-    const pokemon = allPokemons[currentIndex];
+    const pokemon = displayedPokemons[currentIndex];
     const dialog =  document.getElementById('dialog');
     dialog.innerHTML = getDialogPokemonCard(pokemon);
     dialog.showModal();
+    closeOpenDialog();
     await loadEvolutionChain(pokemon)
+}
+
+function closeOpenDialog() {
+    const dialog = document.getElementById('dialog');
+    dialog.addEventListener("click", (event) => {
+        if(event.target === dialog) {
+            dialog.close();
+        }
+    })
 }
 
 async function loadEvolutionChain(pokemon) {
     const speciesResponse = await fetch(pokemon.species.url);
     const speciesData = await speciesResponse.json();
-
     const evolutionResponse = await fetch(speciesData.evolution_chain.url);
     const evolutionData = await evolutionResponse.json();
-
     const names = getEvolutionNames(evolutionData.chain);
-
-    const container =
-        document.getElementById(`evolutionChain${pokemon.id}`);
-
+    const container = document.getElementById(`evolutionChain${pokemon.id}`);
     container.innerHTML = "";
 
     for (const name of names) {
-
         const response = await fetch(BASE_URL + name);
-
         const evolutionPokemon = await response.json();
-
         container.innerHTML += evolutionChain(evolutionPokemon);
     }
 }
 
 function getEvolutionNames(chain) {
-
     const evolutions = [];
-
     while (chain) {
-
         evolutions.push(chain.species.name);
-
         chain = chain.evolves_to[0];
     }
-
     return evolutions;
 }
 
 async function renderDialog() {
     const dialog = document.getElementById('dialog');
-    const pokemon = allPokemons[currentIndex];
+    const pokemon = displayedPokemons[currentIndex];
     dialog.innerHTML = getDialogPokemonCard(pokemon);
     await loadEvolutionChain(pokemon);
 }
@@ -59,28 +56,25 @@ function closeDialog() {
 function arrowLeft() {
     currentIndex--;
     if(currentIndex < 0) {
-        currentIndex = allPokemons.length - 1;
+        currentIndex = displayedPokemons.length - 1;
     }
-
     renderDialog();    
 }
 
 function arrowRight() {
     currentIndex++;
-    if (currentIndex >= allPokemons.length) {
+    if (currentIndex >= displayedPokemons.length) {
         currentIndex = 0;
     }
-    
     renderDialog();
 }
 
 function arrow(currentIndex, direction) {
     currentIndex += direction;
     if(currentIndex < 0) {
-        currentIndex = allPokemons.length - 1;
-    } else if (currentIndex >= allPokemons.length) {
+        currentIndex = displayedPokemons.length - 1;
+    } else if (currentIndex >= displayedPokemons.length) {
         currentIndex = 0;
     }
-
     renderDialog();
 }
